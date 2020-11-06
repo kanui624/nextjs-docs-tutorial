0f05419b621f471ea3adf4775682b4616349ce1b
Kanui <marley.kanui@gmail.com>
2020-11-06 15:04:54 -0600

    nextjs-docs-tut - global commit 04 - Added next Head Component to Edit the title tag of the page

--- pages/posts/firstpost.js
+++ pages/posts/firstpost.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 const firstPost = () => {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
@@ -14,3 +18,11 @@ const firstPost = () => {
 };
 
 export default firstPost;
+
+// If you need to link to an external page outside the Next.js app, just use an <a> tag without Link. If you need to add attributes like, for example, className, add it to the a tag, not to the Link tag.
+
+// Next.js does code splitting automatically, so each page only loads what’s necessary for that page.
+
+// Client-side navigation means that the page transition happens using JavaScript, which is faster than the default navigation done by the browser.
+
+// in a production build of Next.js, whenever Link components appear in the browser’s viewport, Next.js automatically prefetches the code for the linked page in the background. By the time you click the link, the code for the destination page will already be loaded in the background, and the page transition will be near-instant!
