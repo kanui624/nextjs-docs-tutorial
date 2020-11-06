7210474eeac5430c803da961d7b74e6720fa66d9
Kanui <marley.kanui@gmail.com>
2020-11-06 15:22:35 -0600

    nextjs-docs-tut - global commit 06 - Added _app.js file and global.css file for app-wide styling

--- components/Layouts.jsx
+++ components/Layouts.jsx
@@ -1,6 +1,8 @@
 // CSS
 import styles from './layout.module.css';
 
-export default function Layout({ children }) {
+const Layout = ({ children }) => {
   return <div className={styles.container}>{children}</div>;
-}
+};
+
+export default Layout;
---
+++ pages/_app.jsx
@@ -0,0 +1,8 @@
+// CSS
+import '../styles/gloabl.css';
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
