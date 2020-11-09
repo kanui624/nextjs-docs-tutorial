68881bd2b0c0f0f18caa261956c8e46407adbddd
Kanui <marley.kanui@gmail.com>
2020-11-09 09:09:18 -0600

    nextjs-docs-tut - global commit 08 - Pre-Rendering

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Home = () => {
         <p>I'm Marley</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <a href="https://nextjs.org/learn">our Next.js tutorial!</a>.)
         </p>
       </section>
     </Layout>
